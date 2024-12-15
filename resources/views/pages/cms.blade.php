f00c0f8be0b4ade573004ac08892f8170e6d9f43
SHiv <cu.gagankashyap@gmail.com>
2024-12-15 17:19:04 +0530

    Images

--- resources/views/pages/cms.blade.php
+++ resources/views/pages/cms.blade.php
@@ -197,36 +197,41 @@
             </div>
           </div>
         <div class="container">
-          <div class="row mt-2 mb-2">
-            <div class="col-md-12 col-lg-12 col-sm-12">
-              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                  <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                  </ol>
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="img-fluid w-100 custom-img" src="{{asset('images/ShresthaSharma.jpeg')}}" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="img-fluid w-100 custom-img" src="{{asset('images/ShresthaSharma.jpeg')}}" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="img-fluid w-100 custom-img" src="{{asset('images/ShresthaSharma.jpeg')}}" alt="Third slide">
+          @if(null!==(module(4)))
+            <div class="gallery-wrap">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="row">
+                      @foreach(module(4) as $gallery)
+                      @if(strtolower(str_replace(' ', '-', $gallery->title)) == 'news')
+                      <div class="col-lg-3 col-md-6">
+                        <div class="galleryImg"><img src="{{asset('images/thumb/'.$gallery->image)}}" alt="{{$gallery->title}}" style=" border-radius: 12px;    height: 170px;">
+                          <div class="portfolio-overley">
+                            <div class="content"> <a href="{{asset('images/'.$gallery->image)}}" class="fancybox image-link" data-fancybox="images" title="{{$gallery->title}}"><i class="fas fa-search-plus"></i></a> </div>
+                          </div>
+                        </div>
+                      </div>
+                      @endif
+                      @endforeach
+                      
                     </div>
                   </div>
-                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
                 </div>
+              </div>
             </div>
-          </div>
+            @endif
+
+            <div class="innerContent-wrap">
+              <div class="container">
+                <div class="cont_info ">
+                  <div class="row">
+                  
+                  </div>
+                </div>
+                
+              </div>
+            </div>
           <div class="row mt-2 mb-2">
               <div class="col-lg-6 col-sm-12" style="border: 5px solid #232936;">
                   <h3>Recent News</h3>
